26bbba5dff277ac7adada95d0a7fdb686c95b569
mike-pinchuk <morning.sunshine.work@gmail.com>
2020-12-30 11:24:12 +0200

    change some features

--- app.js
+++ app.js
@@ -9,20 +9,26 @@ const data = [store_1, store_2, store_3]
 let file = []
 
 const recoder = (source, fields) => {
-    for(let i = 0; i < source.length; i++) {
-        const csv = JSONtoCSV(source[i], {fields: fields})
-        fs.writeFileSync('./testing.csv', csv)
+    for (let i = 0; i < source.length; i++) {
+        const csv = JSONtoCSV(source[0], { fields: fields })
+        const writer = fs.createWriteStream('./sample.csv')
+        writer.write(csv, err => {
+            if (err) {
+                console.log('Error')
+            }
+            console.log('Success')
+        })
     }
 }
-    
-const controll = data => {
-        let array = []
-        data.map((e, i) => {
-            file.push(Object.keys(e))
-            array = file[0].concat(file[i])    
-        })
 
-        recoder(data, array)
+const controll = data => {
+    let array = []
+    data.map((e, i) => {
+        file.push(Object.keys(e))
+        array = array.concat(file[i])
+    })
+
+    recoder(data, array)
 }
 
 controll(data)
