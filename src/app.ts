067ab0f212cf9c102d6548ef35eee40cf7ad4f13
mike-pinchuk <morning.sunshine.work@gmail.com>
2020-12-31 14:43:38 +0200

    work on fix bug

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,20 @@ import store_1 from '../data1.json'
 import store_2 from '../data2.json'
 import store_3 from '../data3.json'
 
-type EmptyArray = []
-
 const data: Array<object> = [store_1, store_2, store_3]
 
-let file: EmptyArray = []
+let file: Array<string[]> = []
 
 const controll = (data: Array<object>) => {
-    let array: EmptyArray = []
-    let filteredArray: EmptyArray = []
+    let array: Array<string> = []
+    let filteredArray: Array<string> = []
 
     data.map((elem, i) => {
         file.push(Object.keys(elem))
         array = array.concat(...file[i])
     })
 
-    for (let str: String in array) {
+    for (let str in array) {
         if (!filteredArray.includes(str)) {
             filteredArray.push(str)
         }
@@ -28,7 +26,7 @@ const controll = (data: Array<object>) => {
     mergeJSON(data, filteredArray)
 }
 
-const mergeJSON = (arrayJSON: Array<any>, transferData: Array<any>) => {
+const mergeJSON = (arrayJSON: Array<any>, transferData: string) => {
     const resourse: Object = arrayJSON.map((elem, i) => {
         for (let keys in elem[i]) {
             if (elem[transferData] !== elem[keys]) {
@@ -41,7 +39,7 @@ const mergeJSON = (arrayJSON: Array<any>, transferData: Array<any>) => {
     recorder(resourse, transferData)
 }
 
-const recorder = (sourse: Object, fields: Array<any>) => {
+const recorder = (sourse: Object, fields: Array<string>) => {
     const csv: Object = parse(sourse, { fields: fields })
     const writer = fs.createWriteStream('./sample.csv')
     writer.write(csv, err => {
