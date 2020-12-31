598bd245c19c6ed60e17f99039bfd2a80f86621c
mike-pinchuk <morning.sunshine.work@gmail.com>
2020-12-31 14:15:05 +0200

    redo on typescript format

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const JSONtoCSV = require('json2csv').parse
-const store_1 = require('./data1.json')
-const store_2 = require('./data2.json')
-const store_3 = require('./data3.json')
+const store_1 = require('../data1.json')
+const store_2 = require('../data2.json')
+const store_3 = require('../data3.json')
 
 const data = [store_1, store_2, store_3]
 
---
+++ src/app.ts
@@ -0,0 +1,55 @@
+import * as fs from 'fs'
+import * as JSONtoCSV from 'json2csv'
+import store_1 from '../data1.json'
+import store_2 from '../data2.json'
+import store_3 from '../data3.json'
+
+type EmptyArray = []
+
+const data: Array<object> = [store_1, store_2, store_3]
+
+let file: EmptyArray = []
+
+const controll = (data: Array<object>) => {
+    let array: EmptyArray = []
+    let filteredArray: EmptyArray = []
+
+    data.map((elem, i) => {
+        file.push(Object.keys(elem))
+        array = array.concat(...file[i])
+    })
+
+    for (let str: String in array) {
+        if (!filteredArray.includes(str)) {
+            filteredArray.push(str)
+        }
+    }
+
+    mergeJSON(data, filteredArray)
+}
+
+const mergeJSON = (arrayJSON: Array<any>, transferData: Array<any>) => {
+    const resourse: Object = arrayJSON.map((elem, i) => {
+        for (let keys in elem[i]) {
+            if (elem[transferData] !== elem[keys]) {
+                elem[keys] = elem[keys]
+            }
+        }
+        return elem
+    })
+
+    recorder(resourse, transferData)
+}
+
+const recorder = (sourse: Object, fields: Array<any>) => {
+    const csv: Object = JSONtoCSV.parse(sourse, { fields: fields })
+    const writer = fs.createWriteStream('./sample.csv')
+    writer.write(csv, err => {
+        if (err) {
+            console.log('Error')
+        }
+        console.log('Success')
+    })
+}
+
+console.log(data)
